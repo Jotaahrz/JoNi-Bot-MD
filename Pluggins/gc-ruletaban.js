3c37678a432ede5a91c4e89d7bb70253a2624949
Jota 🐼 <rodriguezjota90@gmail.com>
2026-07-12 19:10:42 -0500

    Actualizar gc-ruletaban.js

--- Pluggins/gc-ruletaban.js
+++ Pluggins/gc-ruletaban.js
@@ -3,22 +3,19 @@ let handler = async (m, { conn, participants }) => {
   let users = participants.filter(p => !p.admin && p.id !== conn.user.jid).map(p => p.id)
 
   if (users.length === 0) {
-    return conn.reply(m.chat, 'No hay usuarios disponibles para eliminar 🎯', m)
+    return conn.reply(m.chat, '*No hay usuarios disponibles para eliminar* 🎯', m)
   }
 
-  // Elegir uno al azar
   let elegido = users[Math.floor(Math.random() * users.length)]
 
-  // Avisar al grupo
-  await conn.reply(m.chat, `🎡 La ruleta giró... y salió: @${elegido.split('@')[0]} 😈`, m, {
+  await conn.reply(m.chat, `🎡 *La ruleta giró... y salió:* @${elegido.split('@')[0]} 😈\n> serás eliminadx del grupo.`, m, {
     mentions: [elegido]
   })
 
-  // Eliminar al usuario
   try {
     await conn.groupParticipantsUpdate(m.chat, [elegido], 'remove')
   } catch (e) {
-    conn.reply(m.chat, 'No pude eliminar al usuario, revisa permisos ⚠️', m)
+    conn.reply(m.chat, '*No pude eliminar al usuario, revisa permisos* ⚠️', m)
   }
 }
 
